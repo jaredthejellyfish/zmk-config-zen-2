5a4bb17fb76b73017e3c1aa511f1c3c437581c3d
Jared Hernandez <ger.almenara@gmail.com>
2025-07-04 04:02:10 -0400

    Remove zenlogo from custom status screen implementation to streamline display functionality.

--- config/custom_status_screen.c
+++ config/custom_status_screen.c
@@ -14,7 +14,6 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
-LV_IMG_DECLARE(zenlogo);
 LV_IMG_DECLARE(layers2);
 
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_BATTERY_STATUS)
@@ -65,13 +64,6 @@ lv_obj_t *zmk_display_status_screen()
     lv_obj_set_style_text_font(zmk_widget_layer_status_obj(&layer_status_widget),
                                &lv_font_montserrat_16, LV_PART_MAIN);
     lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_BOTTOM_MID, 0, -5);
-#endif
-
-#if !IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
-    lv_obj_t *zenlogo_icon;
-    zenlogo_icon = lv_img_create(screen);
-    lv_img_set_src(zenlogo_icon, &zenlogo);
-    lv_obj_align(zenlogo_icon, LV_ALIGN_BOTTOM_MID, 0, -5);
 #endif
 
     return screen;
